54fb20f6dbdc6a8e685f8e569f70467d35697094
ROM <rom.void95@gmail.com>
2022-09-20 02:21:51 -0500

    Update SuggestionsHelper.java

--- src/main/java/com/readonlydev/util/discord/SuggestionsHelper.java
+++ src/main/java/com/readonlydev/util/discord/SuggestionsHelper.java
@@ -20,8 +20,8 @@ import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 @UtilityClass
 public class SuggestionsHelper
 {
-    
-    public List<String> getAllAuthors() 
+
+    public List<String> getAllAuthors()
     {
         DBGalacticBot db = BotData.database().botDatabase();
         return db.getManager().getList().stream().map(Suggestion::getAuthorId).collect(Collectors.toList());
@@ -31,26 +31,26 @@ public class SuggestionsHelper
     {
         LinkedMessages messages = suggestion.getMessages();
         Message populatPostMsg = null;
-        
-        if(messages.postMsg().isPresent())
+
+        if (messages.postMsg().isPresent())
         {
             setStatusOnMessage(messages.postMsg().get(), status);
-            
-            if(messages.communityPopularMsg().isPresent())
+
+            if (messages.communityPopularMsg().isPresent())
             {
                 setStatusOnMessage(messages.communityPopularMsg().get(), status);
-                
-                if(messages.devPopularMsg().isPresent())
+
+                if (messages.devPopularMsg().isPresent())
                 {
                     setStatusOnMessage(messages.devPopularMsg().get(), status);
                     populatPostMsg = messages.communityPopularMsg().get();
                 }
             }
         }
-        
+
         return populatPostMsg;
     }
-    
+
     private static void setStatusOnMessage(Message message, SuggestionStatus status)
     {
         MessageEmbed suggestionEmbed = message.getEmbeds().get(0);
@@ -63,19 +63,24 @@ public class SuggestionsHelper
             .addField(suggestionEmbed.getFields().get(0))
             .addField(status.getStatusEmbedField());
         //@format
-        
+
         message.editMessageEmbeds(builder.build()).queue();
     }
 
     public static void handleSuggestionUpvoteEvent(MessageReactionAddEvent event)
     {
-       DBGalacticBot suggestions = BotData.database().botDatabase();
+        DBGalacticBot suggestions = BotData.database().botDatabase();
 
         if (suggestions.getAllSuggestionMessageIds().contains(event.getMessageId()))
         {
             Suggestion suggestion = suggestions.getSuggestionFromMessageId(event.getMessageId());
             Message message = DiscordUtils.getMessageOrNull(event);
-            
+
+            if (event.getUser().getId().equals(suggestion.getAuthorId()))
+            {
+                event.retrieveMessage().flatMap((m) -> m.removeReaction(event.getEmoji(), event.getUser())).queue();
+            }
+
             if (message != null && !event.getUser().getId().equals(suggestion.getAuthorId()))
             {
                 int reactionCount = message.getReactions().get(0).getCount() - 1;
@@ -88,10 +93,12 @@ public class SuggestionsHelper
                 {
                     TextChannel popularChannel = GalacticBot.getJda().getTextChannelById(options.getPopularChannelId());
                     TextChannel devPopularChannel = GalacticBot.getJda().getTextChannelById(options.getDevServerPopularChannelId());
-                    
-                    popularChannel.sendMessageEmbeds(message.getEmbeds().get(0)).queue(s1 -> {
+
+                    popularChannel.sendMessageEmbeds(message.getEmbeds().get(0)).queue(s1 ->
+                    {
                         String communityServerMsgId = s1.getId();
-                        devPopularChannel.sendMessageEmbeds(message.getEmbeds().get(0)).queue(s2 -> {
+                        devPopularChannel.sendMessageEmbeds(message.getEmbeds().get(0)).queue(s2 ->
+                        {
                             String devServerMsgId = s2.getId();
                             suggestions.addNewPopularLinkedMessages(communityServerMsgId, devServerMsgId, suggestion);
                         });
